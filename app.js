2200dfcd27c38fc74ae668ae2d7fd45c9c72f62b
Priya <PriyaDoIT@users.noreply.github.com>
2018-01-15 13:41:01 -0600

    remove console.logs for js file

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var database = firebase.database();
 $(".add-train-btn").on("click", function(event) {
   event.preventDefault();
 
+  //take form inputs and store in variables
   var train = $("#name-input").val().trim();
   var destination = $("#destination-input").val().trim();
   var frequency = $("#frequency-input").val().trim();
@@ -28,41 +29,26 @@ $(".add-train-btn").on("click", function(event) {
 
   database.ref().push(newTrain);
 
-  console.log(newTrain.train);
-  console.log(newTrain.destination);
-  console.log(newTrain.frequency);
-  console.log(newTrain.firstTime);
-
+// Clear form inputs 
 $("#name-input").val("");
 $("#destination-input").val("");
 $("#frequency-input").val("");
 $("#first-input").val("");
 });
 database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-  console.log(childSnapshot.val());
 
   var train = childSnapshot.val().train;
   var destination = childSnapshot.val().destination;
   var frequency = childSnapshot.val().frequency;
   var firstTime = childSnapshot.val().firstTime;
 
-  // Train Info
-  console.log(train);
-  console.log(destination);
-  console.log(frequency);
-  console.log(firstTime);
-
   // first Train pushed back to make sure it comes before current time
   var firstTimeConverted = moment(firstTime, "hh:mm a").subtract(1, "years");
-  console.log(firstTimeConverted);
   var currentTime = moment().format("HH:mm a");
-  console.log("CURRENT TIME: " + currentTime);
   // store difference between currentTime and first train converted in a variable.
   var timeDiff = moment().diff(moment(firstTimeConverted), "minutes");
-  console.log("Difference in Time: " + timeDiff);
   // find Remainder of the time left and store in a variable
   var timeRemainder = timeDiff % frequency;
-  console.log("Time Remainder: " +timeRemainder);
   // to calculate minutes till train,we store it in a variable
   var minToTrain = frequency - timeRemainder;
   // next train
